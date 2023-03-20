86ccd1130b0486b1095bfb102bf6447e3de28b27
dom <davide.domini2@unibo.it>
2023-03-20 14:59:05 +0100

    fix(03): fix obstacle avoidance

--- 03/ex1.lua
+++ 03/ex1.lua
@@ -5,6 +5,8 @@ MAX_VELOCITY = 10
 PROX_THRESHOLD = 0.4
 Dv = 15
 n_steps = 0
+n_steps_avoid = 0
+avoiding = false
 
 
 function level_random_walk()
@@ -52,34 +54,49 @@ end
 
 function level_obstacle_avoidance()
     left_v, right_v = level_phototaxis()
-    max = 0
-	i_max = 1
-	for i=1,24 do
-		if robot.proximity[i].value > max then
-			max = robot.proximity[i].value
-			i_max = i
+    max = -1
+	i_max = -1
+    proximity_sensor_to_use = { 1, 2, 3, 4, 5, 6, 24, 23, 22, 21, 20, 19 }
+	for i=1,#proximity_sensor_to_use do
+        index = proximity_sensor_to_use[i]
+		if robot.proximity[index].value > max then
+			max = robot.proximity[index].value
+			i_max = index
 		end
 	end
 
 	if max >= PROX_THRESHOLD then
+        -- need to avoid
         log("AVOID --- inibisco LIGHT")
-        n_steps = n_steps + 1
-        if n_steps == 0 then
-            if i_max >= 1 and i_max <= 6 and max >= PROX_THRESHOLD then
+        n_steps_avoid = 0
+        avoiding = true
+        if (i_max >= 1) and (i_max <= 6) then
             -- Turn right
-                log("[AVOID] -- turn right")
-                left_v = Dv
-                right_v = -Dv
-            elseif  i_max >= 19 and i_max <= 24 and max >= PROX_THRESHOLD then
-                -- Turn left
-                log("[AVOID] -- turn left")
-                left_v = -Dv
-                right_v = Dv
-            end
-        elseif n_steps == MOVE_STEPS then
-            n_steps = 0
+            log("[AVOID] -- turn right")
+            left_v = Dv
+            right_v = -Dv
+        elseif (i_max >= 19) and (i_max <= 24) then
+            -- Turn left
+            log("[AVOID] -- turn left")
+            left_v = -Dv
+            right_v = Dv
+        end
+        n_steps_avoid = n_steps_avoid + 1
+	end
+
+	if (avoiding == true) and (max < PROX_THRESHOLD) then
+
+        if (n_steps_avoid % MOVE_STEPS) == 0 then
+            n_steps_avoid = 0
+            avoiding = false
+        else
+            log("[AVOID] -- keep going straight")
+            left_v = Dv
+            right_v = Dv
+            n_steps_avoid = n_steps_avoid + 1
         end
 	end
+
     return left_v, right_v
 end
 
@@ -112,6 +129,8 @@ function reset()
 	right_v = robot.random.uniform(0,MAX_VELOCITY)
 	robot.wheels.set_velocity(left_v,right_v)
 	n_steps = 0
+	n_steps_avoid = 0
+	avoiding = false
 end
 
 
